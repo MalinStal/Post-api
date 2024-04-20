47faa40ca10b586bba9aeb24da9e70565dd9ad8b
MalinStal <malinstal.94@gmail.com>
2024-04-20 14:17:23 +0200

    created vit project fore frontend

--- Post_api/obj/Debug/net8.0/Post_api.AssemblyInfo.cs
+++ Post_api/obj/Debug/net8.0/Post_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Post_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf056e2e116777b25a9dd6ce8465d90360c44df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac826e80c8e122250aedd80d5c804624c55fd14d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Post_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Post_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
